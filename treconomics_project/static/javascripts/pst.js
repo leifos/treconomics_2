c21fb360222ef5d3954d6e1ff5d496ae4246a9e7
Leifos <leifos@acm.org>
2019-12-05 10:44:02 +0000

    Updated the PST A test will all the words.. three pages full.

--- treconomics_project/static/javascripts/pst.js
+++ treconomics_project/static/javascripts/pst.js
@@ -8,35 +8,54 @@ $(document).ready(function(){
     function finished(){
     $('#pst_test').fadeOut(100);
     $('#nextbtn').show();
+    alert("Times up for completing the perceptual speed test - click next to continue.")
     };
 
     $('#pst_test').hide();
     $('#nextbtn').hide();
     $('#pst1').hide();
     $('#pst2').hide();
-
+    $('#pst3').hide();
 
  $('#btn1').click(function() {
     $('#pst1').show();
     $('#pst2').hide();
+    $('#pst3').hide();
+            $('#btn1').hide();
+            $('#btn2').show();
+            $('#btn3').show();
 
  });
 
  $('#btn2').click(function() {
     $('#pst2').show();
     $('#pst1').hide();
-
+    $('#pst3').hide();
+            $('#btn2').hide();
+             $('#btn1').show();
+            $('#btn3').show();
  });
+
+ $('#btn3').click(function() {
+    $('#pst2').hide();
+    $('#pst1').hide();
+    $('#pst3').show();
+            $('#btn3').hide();
+             $('#btn2').show();
+            $('#btn1').show();
+ });
+
 
 
      $('#startbtn').click(function() {
         $('#pst_test').show();
-        $('#pst1').show()
+        $('#pst1').show();
+        $('#btn1').hide();
         $('#startbtn').hide();
         $('#pst_instructions').hide();
 
 
-        setTimeout(finished, 30000)
+        setTimeout(finished, 120000)
      });
 
 
