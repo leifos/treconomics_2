09530535d13e09fa0f3e4b21b9899dafd00676da
David Maxwell <maxwelld90@gmail.com>
2019-12-23 12:33:13 +0000

    Added Bash script for resetting the DO droplet

---
+++ treconomics_project/clean_droplet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+./clean_start.sh
+
+echo Need root privileges now... be prepared to enter the sudo password.
+
+sudo chown :www-data experiment.log
+sudo chown :www-data treconomics.db
+chmod 664 experiment.log
+chmod 664 treconomics.db
+
+echo You should now be good to go.
