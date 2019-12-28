3514fb47d6404d36d7028b50a43519ef211931d4
leifos <leifos@acm.org>
2019-12-28 15:12:59 +0000

    updated the templates to be more clear about the reward instructions

--- treconomics_project/survey/admin.py
+++ treconomics_project/survey/admin.py
@@ -5,13 +5,25 @@ from survey.models import PSTCharSearch, PSTNumberSearch, PersonalitySurvey, Sys
 from survey.models import PostPerceptionSurvey, DemographicsSurvey, ConceptListingSurvey
 from survey.models import PreTaskTopicKnowledgeSurvey, PostTaskTopicRatingSurvey
 
-admin.site.register(PSTCharSearch)
-admin.site.register(PSTNumberSearch)
-admin.site.register(PersonalitySurvey)
-admin.site.register(SystemSurvey)
-admin.site.register(PostPerceptionSurvey)
-admin.site.register(DemographicsSurvey)
-admin.site.register(OverallInterview)
-admin.site.register(ConceptListingSurvey)
-admin.site.register(PreTaskTopicKnowledgeSurvey)
-admin.site.register(PostTaskTopicRatingSurvey)
+class PSTAdmin(admin.ModelAdmin):
+    fields = ['user', 'correct', 'incorrect']
+    list_display = ('user', 'correct', 'incorrect')
+
+
+class GenericUserAdmin(admin.ModelAdmin):
+    list_display = ['user']
+
+class GenericUserTopicAdmin(admin.ModelAdmin):
+    list_display = ['user', 'task_id', 'topic_num']
+
+
+admin.site.register(PSTCharSearch, PSTAdmin)
+admin.site.register(PSTNumberSearch, PSTAdmin)
+admin.site.register(PersonalitySurvey,GenericUserAdmin)
+admin.site.register(SystemSurvey, GenericUserTopicAdmin)
+admin.site.register(PostPerceptionSurvey, GenericUserTopicAdmin)
+admin.site.register(DemographicsSurvey, GenericUserAdmin)
+admin.site.register(OverallInterview, GenericUserAdmin)
+admin.site.register(ConceptListingSurvey, GenericUserTopicAdmin)
+admin.site.register(PreTaskTopicKnowledgeSurvey, GenericUserTopicAdmin)
+admin.site.register(PostTaskTopicRatingSurvey, GenericUserTopicAdmin)
