9aac51e0bc43ea1625b7f2022a8e700aa7f4b6d6
David Maxwell <maxwelld90@gmail.com>
2019-12-28 14:23:57 +0000

    Updated clean scripts to include a keepdata option -- does not delete the database and log

--- treconomics_project/clean_droplet.sh
+++ treconomics_project/clean_droplet.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-./clean_start.sh
+if [ "$1" != "keepdata" ]; then
+    echo "You do not want to keep ANY data. The log and database will be wiped if you continue."
+    echo "If you want to keep data, run this script with ./clean_droplet.sh keepdata"
+    read -p "Do you want to continue? [Y/N]" -n 1 -r
+    echo
+
+    if [[ $REPLY =~ ^[Nn]$ ]]; then
+        exit 1
+    fi
+fi
+
+./clean_start.sh $1
 
 echo Need root privileges now... be prepared to enter the sudo password.
 
--- treconomics_project/clean_start.sh
+++ treconomics_project/clean_start.sh
@@ -1,16 +1,24 @@
-rm treconomics.db
-rm experiment.log
+#!/bin/bash
+
+if [ "$1" != "keepdata" ]; then
+    rm treconomics.db
+    rm experiment.log
+fi
+
 rm survey/migrations/*
 rm survey/migrations/__pycache__/*
 rm treconomics/migrations/*
 rm treconomics/migrations/__pycache__/*
-python manage.py makemigrations
-python manage.py makemigrations treconomics
-python manage.py makemigrations survey
-python manage.py migrate
-python manage.py migrate treconomics
-python manage.py migrate survey
-python manage.py createsuperuser
-python populate_treconomics_db.py
-python populate_ads.py
-python populate_users.py
+
+if [ "$1" != "keepdata" ]; then
+    python manage.py makemigrations
+    python manage.py makemigrations treconomics
+    python manage.py makemigrations survey
+    python manage.py migrate
+    python manage.py migrate treconomics
+    python manage.py migrate survey
+    python manage.py createsuperuser
+    python populate_treconomics_db.py
+    python populate_ads.py
+    python populate_users.py
+fi
